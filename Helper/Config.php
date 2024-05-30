39fe9cde16ed851442bf6860c77cc2d9e039934a
spyrmpontinis <spyrmpontinis@gmail.com>
2024-05-30 14:48:25 +0300

    Adding WebsiteOptions on Ui Factory

---
+++ Helper/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Onecode\Base\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Store\Model\ScopeInterface;
+
+class Config extends AbstractHelper implements ScopeInterface
+{
+
+}
---
+++ Plugin/OrdersGrid.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Onecode\Base\Plugin;
+
+use Magento\Framework\Data\Collection;
+use Magento\Framework\View\Element\UiComponent\DataProvider\CollectionFactory;
+use Onecode\Base\Helper\Config;
+class OrdersGrid
+{
+
+    /**
+     * @var Config
+     */
+    private $config;
+
+    public function __construct(Config $config)
+    {
+        $this->config = $config;
+    }
+    /**
+     * @param CollectionFactory $subject
+     * @param Collection $result
+     * @param string $requestName
+     * @return Collection
+     */
+    public function afterGetReport(CollectionFactory $subject, Collection $result, $requestName): Collection
+    {
+        if ($requestName !== 'sales_order_grid_data_source') {
+            return $result;
+        }
+        if($this->config->isModuleOutputEnabled('Onecode_TaxydromikiVoucherCreator') ||
+            $this->config->isModuleOutputEnabled('Onecode_SpeedexVoucherCreator')) {
+            return $result;
+        }
+
+        if ($result->getMainTable() === $result->getResource()->getTable('sales_shipment_grid')) {
+            $storesTable = $result
+                ->getResource()
+                ->getTable('store');
+            $result->getSelect()->joinLeft(
+                ['st' => $storesTable],
+                'st.store_id = main_table.store_id',
+                ["website_id"]
+            );
+            $result->getSelect()->group('main_table.entity_id');
+        }
+        return $result;
+    }
+}
